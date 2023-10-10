9a0b0bb886b6d1348cdd22ce8eab1769af76f4bd
YongWon <th9739@naver.com>
2023-10-10 09:04:25 +0900

    dd

--- 13_STL/13_STL/STL.cpp
+++ 13_STL/13_STL/STL.cpp
@@ -1,5 +1,19 @@
 #include<stdio.h>
 #include<vector>
+#include<list>
+
+
+struct stMonster
+{
+	int index = 0;
+
+
+};
+
+bool IsFindMonIndex(const stMonster& mon)
+{
+	return mon.index == 3;//mon 인덱스가 3이면 true, 아니면 false 반환
+}
 
 
 int main()
@@ -43,50 +57,50 @@ int main()
 	//}
 
 
-	std::vector<int> vecint1{ 0,1,2,3,4,5,6,7,8,9 };
-	std::vector<int>::iterator veciter = vecint1.begin();//vector 포인터 만드는법
-	veciter++;
-	veciter+=2;
-	veciter--;
-	printf("veciter=%d\n", *veciter);
+	//std::vector<int> vecint1{ 0,1,2,3,4,5,6,7,8,9 };
+	//std::vector<int>::iterator veciter = vecint1.begin();//vector 포인터 만드는법
+	//veciter++;
+	//veciter+=2;
+	//veciter--;
+	//printf("veciter=%d\n", *veciter);
 
-	vecint1.push_back(100);//끝에 추가
-	for (std::vector<int>::iterator Iter = vecint1.begin(); Iter != vecint1.end(); ++Iter)
-	{
-		printf("Iter=%d\n", *Iter);
-	}
+	//vecint1.push_back(100);//끝에 추가
+	//for (std::vector<int>::iterator Iter = vecint1.begin(); Iter != vecint1.end(); ++Iter)
+	//{
+	//	printf("Iter=%d\n", *Iter);
+	//}
 
-	vecint1.pop_back();//끝에 제거
-	veciter = vecint1.begin();
-	while (veciter != vecint1.end())
-	{
-		printf("veciter=%d\n", *veciter);
-		veciter++;
-	}
-		
-	veciter = vecint1.begin();
-	while (veciter != vecint1.end())
-	{
-		if (*veciter == 5)
-		{
-			veciter = vecint1.erase(veciter);//벡터이터가 가리키는 5를 지운다
-		}
-		else
-		{
-			veciter++;
-		}
-		
-	}
+	//vecint1.pop_back();//끝에 제거
+	//veciter = vecint1.begin();
+	//while (veciter != vecint1.end())
+	//{
+	//	printf("veciter=%d\n", *veciter);
+	//	veciter++;
+	//}
+	//	
+	//veciter = vecint1.begin();
+	//while (veciter != vecint1.end())
+	//{
+	//	if (*veciter == 5)
+	//	{
+	//		veciter = vecint1.erase(veciter);//벡터이터가 가리키는 5를 지운다
+	//	}
+	//	else
+	//	{
+	//		veciter++;
+	//	}
+	//	
+	//}
 
-	veciter = vecint1.begin();
-	while (veciter != vecint1.end())
-	{
-		printf("veciter=%d\n", *veciter);
-		veciter++;
-	}
+	//veciter = vecint1.begin();
+	//while (veciter != vecint1.end())
+	//{
+	//	printf("veciter=%d\n", *veciter);
+	//	veciter++;
+	//}
 
 
-	std::vector<int> v;
+	/*std::vector<int> v;
 	for (int i = 0; i < 15; i++)
 	{
 		v.push_back(i);
@@ -102,6 +116,111 @@ int main()
 	for (int i = 0; i < v.size(); ++i)
 	{
 		printf("v[%d]=%d\n", i, v[i]);
+	}*/
+	
+
+	//std::list<int> listint{ 0,1,2,3,4 };
+	////for문은 안되고, for each는 됨
+	//for (int value : listint)
+	//{
+	//	value = 0;
+	//	printf("listint=%d\n", value);
+	//}
+
+	//for (int value : listint)
+	//{
+	//	printf("listint=%d\n", value);
+	//}
+
+	///*for (int& value : listint)
+	//{
+	//	value = 0;
+	//	printf("listint=%d\n", value);
+	//}*/
+
+	//for (int value : listint)
+	//{
+	//	printf("listint=%d\n", value);
+	//}
+
+	//for (const int& value : listint)
+	//{
+	//	//value = 0;
+	//	printf("listint=%d\n", value);
+	//}
+
+
+	/*std::list<int>::iterator listiter = listint.begin();
+	printf("\nlistiter=%d\n", *listiter);
+	for (std::list<int>::iterator i = listint.begin() ; i != listint.end(); ++i)
+	{
+		printf("\nlistiter[]=%d\n", *i);
+	}*/
+
+
+	std::list<int> listint2{ 20,43,67,2,98 };
+	/*listint2.push_back(100);
+	listint2.push_front(200);
+
+	for (const int& value : listint2)
+	{
+		printf("listint2=%d\n", value);
+	}
+
+	listint2.pop_back();
+	listint2.pop_front();
+
+	for (const int& value : listint2)
+	{
+		printf("listint2=%d\n", value);
+	}*/
+	
+	//std::list<int>::iterator it = std::find(listint2.begin(), listint2.end(), 43);
+	//if (it != listint2.end())
+	//{
+	//	printf("listint2=%d\n", *it);
+	//	//listint2.erase(it);//특정원소 제거
+	//	listint2.insert(it, 2000);//검색한 배열값 앞에 특정값 끼워넣기
+	//	listint2.emplace(it, 2001);//검색한 배열값 앞에 특정값 끼워넣기
+	//}
+	//else
+	//{
+	//	printf("listint2=fail\n");
+	//}
+
+	//for (const int& value : listint2)
+	//{
+	//	printf("listint2=%d\n", value);
+	//}
+
+
+	std::list<stMonster> arrMon;
+
+	for (int i = 0; i < 5; ++i)
+	{
+		stMonster temp;
+		temp.index = i;
+		arrMon.push_back(temp);
 	}
 	
+	for (const stMonster& value : arrMon)
+	{
+		printf("Monster index=%d\n", value.index);
+	}
+
+	std::list<stMonster>::iterator moniter;
+	//moniter = std::find_if(arrMon.begin(), arrMon.end(), IsFindMonIndex);
+	moniter = std::find_if(arrMon.begin(), arrMon.end(), [](const stMonster& mon){ return mon.index == 12; });//함수를 직접 이름 없이 만들어서 찾기 진행함
+
+	
+	if (moniter != arrMon.end())
+	{
+		printf("moniter index=%d\n", moniter->index);
+	}
+	else
+	{
+		printf("moniter find fail\n");
+	}
+	
+
 }
---
+++ Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,370 @@
+#include <stdio.h>
+#include <iostream>
+
+//int cSum(int a, int b) {
+//    return a + b;
+//}
+//
+//int cMinus(int a, int b) {
+//    return a - b;
+//}
+//
+//int cMulti(int a, int b) {
+//    return a * b;
+//}
+//
+//float cDivide(float a, float b) {
+//    return a / b;
+//}
+//
+//enum InputType
+//{
+//    Quit = 0,
+//    Sum,
+//    Minus,
+//    Multi,
+//    Divide
+//};
+//
+//void ScanfinputNum(int& a, int& b)
+//{
+//    printf("\n숫자 하나를 입력하세요:");
+//    scanf_s("%d", &a);
+//    printf("\n숫자 하나 더 입력하세요:");
+//    scanf_s("%d", &b);
+//}
+
+
+float standardWeight(float height)
+{
+    int A=0;
+    A= (height - 100) * 0.9;
+    return A;
+}
+
+float GetBMI(float weight, float standardWeight)
+{
+    int B=0;
+    (weight - standardWeight) * 100 / standardWeight;
+    return B;
+}
+
+
+void FailNumber()
+{
+    printf("잘못입력하셨습니다. 다시 입력해주세요.(예시 '-'제외, OOOOOO O******)\n\n");
+}
+
+
+int main()
+{
+   
+    bool NumInput = true;
+
+    while (NumInput)
+    {
+        int Num1;
+        int Num2;
+        int Num3;
+        int Num4;
+
+        printf("[주민번호 출력 시스템]\n");
+        printf("주민번호 뒤 첫째자리까지 입력해주세요.(예시 '-'제외, OOOOOO O******)\n");
+        scanf_s("%02d%02d%02d%01d", &Num1, &Num2, &Num3, &Num4);
+        //printf("%d%d%d-%d******\n", Num1, Num2, Num3, Num4);
+
+        if (Num2 > 12)
+        {
+            FailNumber();
+        }
+        else if (Num3 > 31)
+        {
+            FailNumber();
+        }
+        else if (Num4 > 4)
+        {
+            FailNumber();
+        }
+
+        else
+        {
+            if (Num4 == 1 || Num4 == 3)
+            {
+                printf("%d년 %d월 %d일 남자\n\n", Num1, Num2, Num3);
+            }
+            else if (Num4 == 2 || Num4 == 4)
+            {
+                printf("%d년 %d월 %d일 여자\n\n", Num1, Num2, Num3);
+            }
+        }
+    }
+
+
+
+
+
+
+
+     
+   /*int Bnum1 = 0;
+   int Bnum2 = 0;
+    
+   scanf_s("%06d%07d", &Bnum1, &Bnum2);
+   printf("%06d-%07d\n", Bnum1, Bnum2);*/
+   
+   bool Input = true;
+
+   //while (Input)
+   // {
+   //    printf("주민번호를 입력해주세요.\n");
+   //    int number[13]; // 입력 받을 8개 숫자 배열
+
+   //    for (int i = 0; i < 13; i++)
+   //    {
+   //        scanf_s("%1d", &number[i]);
+   //    }
+
+
+   //    if (number[2] > 1)
+   //     {
+   //         printf("잘못입력하셨습니다.\n");
+   //         return;
+   //     }
+   //     else if (number[3] > 2)
+   //     {
+   //         printf("잘못입력하셨습니다.\n");
+   //         return;
+   //     else if (number[4] > 3)
+   //     {
+   //         printf("잘못입력하셨습니다.\n");
+   //         return;
+   //     else if (number[5] > 4)
+   //     {
+   //         printf("잘못입력하셨습니다.\n");
+   //         return;
+   //     }
+   //     else
+   //         continue;
+
+   //     int d = 0;
+   //     int Num = 0;
+   //     printf("주민번호 뒤 첫째자리를 입력해주세요.\n");
+   //     scanf_s("%d\n", &d);
+
+   //     if (number[7] == 1, 3)
+   //     {
+   //         printf("생년월일 = %d%d년 %d%d월 %d%d일 남자\n", number[0],number[1], number, c);
+   //         return;
+   //     }
+   //     else if (number[7] == 2, 4)
+   //     {
+   //         printf("생년월일 = %d년 %d월 %d일 여자\n", a, b, c);
+   //         return;
+   //     }
+   //     else
+   //         printf("잘못입력하셨습니다.\n");
+   //     return;
+   //     }
+
+   // }
+
+
+    /*bool BMIinput = true;
+
+    while (BMIinput)
+    {
+        
+        int Button = 0;
+
+        printf("BMI를 계산하시겠습니까?\n0:No, 1:Yes\n");
+        scanf_s("%d", &Button);
+
+        switch (Button)
+        {
+        case 0:
+            BMIinput = false;
+            break;
+        case 1:
+            int BMIheight = 0;
+            int BMIweight = 0;
+
+            printf("몸무게를 입력해주세요\n");
+            scanf_s("%d", &BMIheight);
+            standardWeight(BMIheight);
+            printf("키를 입력해주세요\n");
+            scanf_s("%d", &BMIweight);
+            int BMIResult = GetBMI(BMIweight, standardWeight(BMIheight));
+
+            if (BMIResult <= 10)
+            {
+                printf("정상\n");
+            }
+            else if (10 < BMIResult <= 20)
+            {
+                printf("과체중\n");
+            }
+            else if (20 < BMIResult)
+            {
+                printf("비만\n");
+            }
+        }
+
+
+        
+
+    }*/
+
+
+
+
+
+    /*bool SInput = true;
+
+    while (SInput)
+    {
+        int sNum = 0;
+        printf("소득세를 계산하시겠습니까?\n0:No, 1:Yes\n");
+        scanf_s("%d", &sNum);
+        
+
+        switch (sNum)
+        {
+        case 0:
+            SInput = false;
+            break;
+        case 1:
+            int Money = 0;
+            printf("연간 총 소득을 입력하세요(만 단위):\n");
+            scanf_s("%d", &Money);
+            system("cls");
+            int per = 0;
+
+            if (Money > 100000)
+            {
+                per = 45;
+            }
+            else if (50000< Money && Money <= 100000)
+            {
+                per = 42;
+            }
+            else if (30000< Money && Money <= 50000 )
+            {
+                per = 40;
+            }
+            else if (15000< Money && Money <= 30000)
+            {
+                per = 38;
+            }
+            else if (8800< Money && Money <= 15000)
+            {
+                per = 35;
+            }
+            else if (4600< Money && Money <= 8800)
+            {
+                per = 24;
+            }
+            else if (1200< Money && Money <= 4600)
+            {
+                per = 15;
+            }
+            else if (Money <= 1200)
+            {
+                per = 6;
+            }
+            printf("당신의 소득세는 %d퍼센트 입니다.\n", per);
+        }
+    }*/
+    
+
+
+
+
+
+    /*bool nInput = true;
+    while (nInput)
+    {
+        int A = 0;
+        int B = 0;
+        
+        printf("[입력한 두 수 사이의 모든 숫자 더하기 프로그램]\n");
+        printf("숫자 하나를 입력하세요.\n");
+        scanf_s("%d", &A);
+        printf("숫자 하나를 더 입력하세요.\n");
+        scanf_s("%d", &B);
+        system("cls");
+
+        int nResult = 0;
+        if (A < B)
+        {
+            for (int i = A; i <= B; ++i)
+            {
+                nResult += i;
+            }
+            printf("결과=%d\n", nResult);
+        }
+        else if (B < A)
+        {
+            for (int i = B; i <= A; ++i)
+            {
+                nResult += i;
+            }
+            printf("결과=%d\n", nResult);
+        }
+        
+
+    }*/
+
+
+
+
+
+
+
+   
+    //bool bRun = true;
+
+    //while (bRun)
+    //{
+    //    InputType nInput = InputType::Quit;
+    //    int a = 0, b = 0;
+    //    printf("어떤 연산을 하시겠습니까? 0)종료 1)더하기 2)빼기 3)곱하기 4)나누기 : ");
+    //    scanf_s("%d", &nInput);
+
+    //    system("cls");//위에 프린트 된 부분이 한번 출력된 뒤에 없어짐
+
+    //    switch (nInput)
+    //    {
+    //    case InputType::Quit:
+    //        bRun = false;
+    //        break;
+
+    //    case InputType::Sum:
+    //        ScanfinputNum(a, b);
+    //        printf("\nSum 결과는 %d 입니다.\n", cSum(a, b));
+    //        break;
+
+    //    case InputType::Minus:
+    //        ScanfinputNum(a, b);
+    //        printf("\nMinus 결과는 %d 입니다.\n", cMinus(a, b));
+    //        break;
+
+    //    case InputType::Multi:
+    //        ScanfinputNum(a, b);
+    //        printf("\nMulti 결과는 %d 입니다.\n", cMulti(a, b));
+    //        break;
+
+    //    case InputType::Divide:
+    //        ScanfinputNum(a, b);
+    //        printf("\nDivide 결과는 %f 입니다.\n", cDivide(a, b));
+    //        break;
+
+    //    default:
+    //        break;
+    //    }
+    //}
+
+
+
+
+
+}
